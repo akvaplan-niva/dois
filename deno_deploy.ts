c6ded0464c6a623adff15526e20784515c51f380
cnrdh <cnrdh@users.noreply.github.com>
2021-11-26 16:53:01 +0100

    debug

--- deno_deploy.ts
+++ deno_deploy.ts
@@ -2,11 +2,11 @@ import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
 
 async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
-  console.warn(pathname);
-  if (pathname.startsWith("/slim/") && /\.ndjson$/.test(pathname)) {
-    const r = await fetch(pathname);
-    console.warn(r);
-    return new Response(r.body, {
+
+  if (pathname.startsWith("/slim/") && /[12][0-9]{3}\.ndjson/.test(pathname)) {
+    console.warn({ pathname });
+
+    return new Response(pathname, {
       headers: {
         "content-type": "text/plain; charset=utf-8",
       },
@@ -15,10 +15,8 @@ async function handleRequest(request: Request): Promise<Response> {
 
   return new Response(
     `<html>
-      <head><script type="module" src="https://cc-icons.vercel.app/define.js"></script></head>
-      <body>
-        <h1></h1>
-      </body>
+      <head><script type="module"></script></head>
+      <body></body>
     </html>`,
     {
       headers: {
