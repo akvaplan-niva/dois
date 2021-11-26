e2d7bc834b0392c42f851050ea43b258b8b80c89
cnrdh <cnrdh@users.noreply.github.com>
2021-11-26 16:42:05 +0100

    debug

--- deno_deploy.ts
+++ deno_deploy.ts
@@ -2,8 +2,10 @@ import { serve } from "https://deno.land/std@0.116.0/http/server.ts";
 
 async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
+  console.warn(pathname);
   if (pathname.startsWith("/slim/") && /\.ndjson$/.test(pathname)) {
     const r = await fetch(pathname);
+    console.warn(r);
     return new Response(r.body, {
       headers: {
         "content-type": "text/plain; charset=utf-8",
@@ -16,38 +18,6 @@ async function handleRequest(request: Request): Promise<Response> {
       <head><script type="module" src="https://cc-icons.vercel.app/define.js"></script></head>
       <body>
         <h1></h1>
-        <cc-icons
-      license="cc-by"
-      class="slotted"
-      xmlns:cc="http://creativecommons.org/ns#"
-      xmlns:dct="http://purl.org/dc/terms/"
-    >
-      <h2 slot="before">
-        <a
-          rel="cc:attributionURL dct:creator"
-          property="cc:attributionName"
-          href="#https://example.com/someone"
-          >Someone</a
-        >.
-
-        <a
-          property="dct:title"
-          rel="cc:attributionURL"
-          href="#https://example.com/work"
-          >Work</a
-        >
-        (year) is licensed under
-        <a
-          rel="license noopener noreferrer"
-          href="https://creativecommons.org/licenses/by/4.0/"
-          target="_blank"
-          >Creative Commons attribution</a
-        >
-      </h2>
-      <h2 slot="after">
-        <a href="#https://example.com/rights">Rights statement</a>
-      </h2>
-    </cc-icons>
       </body>
     </html>`,
     {
