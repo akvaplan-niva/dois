8042c1f7e0ab19dd7c0ceef170796c8bb0b398ce
cnrdh <cnrdh@users.noreply.github.com>
2023-07-10 16:35:07 +0200

    refactor to kv; add auth

--- deno/deploy.js
+++ deno/deploy.js
@@ -1,35 +1,31 @@
 import { routes } from "./routes.js";
-import { corsHeaders, httpError } from "./response.js";
-import { doimap, initDOIMapFromNDJSONFilesInDir} from "./doi-map.js";
+import { httpError } from "./response.js";
 
-import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.193.0/http/server.ts";
+import {
+  acceptSafe,
+  scryptBasicAuth,
+} from "https://deno.land/x/scrypt_basic_auth@1.0.2/mod.ts";
 
-const validateRequest = ({ request }) => {
-  if (!/(get|head|options)/i.test(request.method)) {
-    return { status: 405 };
+const kv = await Deno.openKv();
+
+serve(async (request) => {
+  const basicAuth = await scryptBasicAuth(request, { accept: acceptSafe });
+  if (!basicAuth.ok) {
+    return basicAuth;
   }
-  return { status: 200 };
-};
-
-
-initDOIMapFromNDJSONFilesInDir({doimap,dir: "slim"})
-
-await serve((request) => {
-  //const { status } = validateRequest({ request });
-  // if (status >= 300) {
-  //   return httpError({ request, status });
-  // }
-  // if (/options/i.test(request.method) {
-  //   return new Response(null, { headers: corsHeaders });
-  // }
   const url = new URL(request.url);
+  const found = { route: false };
+
   for (const { pattern, handler } of routes) {
     const m = pattern.exec(url);
     if (m && m.pathname) {
+      found.route = true;
       const { groups, input } = m.pathname;
-      console.warn(handler);
-      const response = handler({ groups, input, url, request });
-      return response;
+      return handler({ groups, input, url, request, kv });
     }
+  }
+  if (!found.route) {
+    return httpError({ status: 404, request });
   }
 });
